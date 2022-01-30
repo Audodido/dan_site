b2221afea3cac88a3be3c4315b3df05415fc39dc
connor <chanwick@gmail.com>
2022-01-30 14:30:07 -0500

    made responsive

--- static/main.js
+++ static/main.js
@@ -1,56 +1,49 @@
 var buy = "https://thes-ftf-cus.bandcamp.com/album/lousy-w-love"
+var stream = "https://ditto.fm/you-me-and-all-the-salt-in-the-sea"
 var email = "mailto:s.ftf.cus@gmail.com"
 var twitter = "https://twitter.com/TheS_ftF_cus"
 var insta = "https://www.instagram.com/thes_ftf_cus/"
 var facebook = "https://www.facebook.com/thes.ftf.cus/"
+var youtube = "https://www.youtube.com/channel/UCya0_2t5aSa95dki35hLQKQ"
+
+https://www.youtube.com/channel/UCya0_2t5aSa95dki35hLQKQ"
 var h = document.getElementById("date")
-var q = document.getElementById("empty")
+var q = document.getElementById("bar")
 var r = document.getElementById("line2")
 
 
-h.innerHTML += "the s-ft \<br>";
-r.innerHTML += "f - c u s";
-// ----------------------------------------------
-// TEXT COLOR CHANGER
+// h.innerHTML += "the s-ft \<br>";
+// r.innerHTML += "f - c u s";
 
-// var colors = ["silver", "gray", "silver", "grey", "silver"]
-// var colors = ["#8fce00", "#9ad219", "#a5d732", "#b0dc4c", "#bbe166", "#c7e67f"]
-// var currentColor = 0
-// var lis = document.querySelectorAll("h1")
 
-// function changeColor() {
-//     --currentColor
-//     if (currentColor < 0) currentColor = colors.length -1
-//     for (var i = 0; i < lis.length; i++) {
-//       lis[i].style.color = colors[(currentColor +i) % colors.length]
-//     }
-//   }
-  
-// setInterval(changeColor, 200)
-
-// ----------------------------------------------
-// iterate over text so it appears one at time
-
-var string = [buy, email,twitter,insta,facebook]
+var string = [buy,stream,email,twitter,insta,youtube,facebook]
 
 var printCount = 0
 var aNum = ((Math.random()*100) + 50).toFixed();
 
 
+
+
+
 function addText() {
     if (printCount <= string.length) {
-    // for (let i=0; i < string.length; i++) {
         if (string[printCount] == buy) {
-            q.innerHTML += "<a href ="+string[printCount]+" target=\"_blank\">buy vinyl/digi</a>" 
+            q.innerHTML += "<a href ="+string[printCount]+" target=\"_blank\">buy vinyl+digi</a>" 
             q.innerHTML += "<br>"
         } else if (string[printCount] == email) {
             q.innerHTML += "<a href ="+string[printCount]+" target=\"_blank\">email</a>" 
+            q.innerHTML += "<br>"
+        } else if (string[printCount] == stream) {
+            q.innerHTML += "<a href ="+string[printCount]+" target=\"_blank\">stream</a>" 
             q.innerHTML += "<br>"
         } else if (string[printCount] == twitter) {
             q.innerHTML += "<a href ="+string[printCount]+" target=\"_blank\">twitter</a>" 
             q.innerHTML += "<br>"
         } else if (string[printCount] == insta) {
             q.innerHTML += "<a href ="+string[printCount]+" target=\"_blank\">insta</a>" 
+            q.innerHTML += "<br>"
+        } else if (string[printCount] == youtube) {
+            q.innerHTML += "<a href ="+string[printCount]+" target=\"_blank\">youtube</a>" 
             q.innerHTML += "<br>"
         } else if (string[printCount] == facebook) {
             q.innerHTML += "<a href ="+string[printCount]+" target=\"_blank\">facebook</a>"
